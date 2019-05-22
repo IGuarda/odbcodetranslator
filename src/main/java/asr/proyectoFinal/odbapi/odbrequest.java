771da2c21fbff6c0aede07571cba444eea509565
Ignacio <idgg.96@gmail.com>
2019-05-22 16:15:51 +0200

    translate

---
+++ src/main/java/asr/proyectoFinal/odbapi/odbrequest.java
@@ -0,0 +1,7 @@
+package asr.proyectoFinal.odbapi;
+import java.io.BufferedReader;
+/*curl --request GET --url 'https://api.eu.apiconnect.ibmcloud.com/hella-ventures-car-diagnostic-api/api/v1/dtc
+ * ?client_id=398f6f60-8d12-439c-938f-1162405d3d44&client_secret=A8xV8kP0hH7oW8tS2kT4cT5tA2pL2sY4rE5rW6rF0dN6dA1yV4
+ * &code_id=P0001&vin=WBAES26C05D&language=EN' --header 'accept: application/json' --header 'content-type: application/json'
+ */
+ 
---
+++ src/main/java/asr/proyectoFinal/services/Traductor.java
@@ -0,0 +1,49 @@
+package asr.proyectoFinal.services;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+
+public class Traductor
+{
+	public static String translate(String palabra, String sourceModel, String destModel, boolean conversational)
+	{
+		String model;
+		if(sourceModel.equals("en") || destModel.equals("en"))
+		{
+			model=sourceModel+"-"+destModel;
+			if(conversational) 
+				model+="-conversational";
+		}
+		else
+			return translate(translate(palabra,sourceModel,"en",conversational),"en",destModel,conversational); //translate to english, then to dest
+
+		LanguageTranslator languageTranslator = new LanguageTranslator();
+		languageTranslator.setUsernameAndPassword("9063266f-6092-4d36-9045-2f2e76614c7d","j5mqVbQERSzuQuk-btE-CrncvPAShUFsDkDyNQauseaM");
+
+		languageTranslator.setEndPoint("https://gateway.watsonplatform.net/language-translator/api");
+		
+		TranslateOptions translateOptions = new
+		TranslateOptions.Builder()
+		 .addText(palabra)
+		 .modelId(model)
+		 .build();
+
+		TranslationResult translationResult = languageTranslator.translate(translateOptions).execute();
+		
+		System.out.println(translationResult);
+
+		String traduccionJSON = translationResult.toString();
+		JsonParser parser = new JsonParser();
+		JsonObject rootObj =
+		parser.parse(traduccionJSON).getAsJsonObject();
+		JsonArray traducciones = rootObj.getAsJsonArray("translations");
+		String traduccionPrimera = palabra;
+		if(traducciones.size()>0)
+		traduccionPrimera =
+		traducciones.get(0).getAsJsonObject().get("translation").getAsString();
+		return traduccionPrimera;
+	}
+}

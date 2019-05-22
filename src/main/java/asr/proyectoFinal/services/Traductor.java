8afaf45cbf6ea3b24245c98dc9c7a24463e13c3c
Ignacio <idgg.96@gmail.com>
2019-05-22 23:50:38 +0200

    dfhhjdfh

--- src/main/java/asr/proyectoFinal/services/Traductor.java
+++ src/main/java/asr/proyectoFinal/services/Traductor.java
@@ -3,28 +3,39 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.util.Language;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions.Builder;
 import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslateOptions;
 import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
+public class Traductor{
+public static String translate(String palabra){
+	LanguageTranslator service = new LanguageTranslator();
 
-public class Traductor
-{
-	public static String translate(String palabra, String sourceModel, String destModel, boolean conversational)
-	{
-		String model;
-		if(sourceModel.equals("en") || destModel.equals("en"))
-		{
-			model=sourceModel+"-"+destModel;
-			if(conversational) 
-				model+="-conversational";
-		}
-		else
-			return translate(translate(palabra,sourceModel,"en",conversational),"en",destModel,conversational); //translate to english, then to dest
+	service.setUsernameAndPassword("apikey","j5mqVbQERSzuQuk-btE-CrncvPAShUFsDkDyNQauseaM");
 
-		LanguageTranslator languageTranslator = new LanguageTranslator();
-		languageTranslator.setUsernameAndPassword("apikey","j5mqVbQERSzuQuk-btE-CrncvPAShUFsDkDyNQauseaM");
-
-		languageTranslator.setEndPoint("https://gateway-lon.watsonplatform.net/language-translator/api");
+		//languageTranslator.setEndPoint("https://gateway-lon.watsonplatform.net/language-translator/api");
 		 
+	TranslateOptions translateOptions = new TranslateOptions.Builder()
+	 .addText(palabra)
+	 .modelId("en-es")
+	 .build();
+	TranslationResult translationResult = service.translate(translateOptions).execute();
+	System.out.println(translationResult);
+	String traduccionJSON = translationResult.toString();
+	JsonParser parser = new JsonParser();
+	JsonObject rootObj = parser.parse(traduccionJSON).getAsJsonObject();
+	String wordCount = rootObj.get("word_count").getAsString();
+	JsonArray traducciones = rootObj.getAsJsonArray("translations");
+	String traduccionPrimera = palabra;
+	if(traducciones.size()>0)
+		traduccionPrimera =	traducciones.get(0).getAsJsonObject().get("translation").getAsString();
+	return traduccionPrimera;
+	}
+}
+		
+		
+		
+		/*
 		TranslateOptions translateOptions = new
 		TranslateOptions.Builder()
 		 .addText(palabra)
@@ -46,4 +57,4 @@ public class Traductor
 		traducciones.get(0).getAsJsonObject().get("translation").getAsString();
 		return traduccionPrimera;
 	}
-}
+}*/
--- src/main/java/asr/proyectoFinal/servlets/Controller.java
+++ src/main/java/asr/proyectoFinal/servlets/Controller.java
@@ -61,7 +61,7 @@ public class Controller extends HttpServlet {
 					}
 					else
 					{
-						parametro = Traductor.translate(parametro, "es", "en", false);
+						parametro = Traductor.translate(parametro);
 						palabra.setName(parametro);
 						store.persist(palabra);
 					    out.println(String.format("Almacenada la palabra: %s", palabra.getName()));			    	  
